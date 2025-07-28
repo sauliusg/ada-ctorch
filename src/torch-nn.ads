72036702e98da61d1dcecca700b6d4c980e4309c
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2025-07-28 07:04:43 +0000

    Removing the "Ada_" prefix from the Shadow Module types.
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@38 58c103b0-9528-442c-8745-02648d8b579a

--- src/torch-nn.ads
+++ src/torch-nn.ads
@@ -16,20 +16,20 @@ private
    
    type Module_Access is access all Module;
    
-   type Ada_Shadow_Module_Type is null record; -- Declared in full and managed on the C++ side
+   type Shadow_Module_Type is null record; -- Declared in full and managed on the C++ side
    
-   type Ada_Shadow_Module_Access is access Ada_Shadow_Module_Type;
+   type Shadow_Module_Access is access Shadow_Module_Type;
    
    type Module is new Ada.Finalization.Limited_Controlled with record
-      Shadow_Module : Ada_Shadow_Module_Access; 
+      Shadow_Module : Shadow_Module_Access; 
    end record;
    
-   function New_AdaShadowModule (M : Module_Access) return Ada_Shadow_Module_Access
+   function New_AdaShadowModule (M : Module_Access) return Shadow_Module_Access
      with Import => True,
      Convention => CPP,
      External_Name => "new_AdaShadowModule";
    
-   procedure Delete_AdaShadowModule (SM : Ada_Shadow_Module_Access)
+   procedure Delete_AdaShadowModule (SM : Shadow_Module_Access)
      with Import => True,
      Convention => CPP,
      External_Name => "delete_AdaShadowModule";
