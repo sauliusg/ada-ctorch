e5aa52092926b497ca4b778cdf9352ff01081018
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2025-08-24 07:20:53 +0000

    Xommenting out a call that causes exceptions.
    trunk/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@81 58c103b0-9528-442c-8745-02648d8b579a

--- programs/mnist.adb
+++ programs/mnist.adb
@@ -36,7 +36,7 @@ begin
    Copy (X2, T2);
    Copy (T1, X1);
    
-   T1 := Net1.Forward (T1);
+   -- T1 := Net1.Forward (T1); -- This raises exception in C++, forwarded to Ada.
    
    Put_Line ("T1 Refcount: " & Integer'Image (Torch.Tensors.Refcount (T1)));
    Put_Line ("T2 Refcount: " & Integer'Image (Torch.Tensors.Refcount (T2)));
