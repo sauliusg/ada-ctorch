0455e6e475023960c90d8e1e7d7a32a53e0149fe
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-03-07 10:25:09 +0000

    Compiling glue for the "Conv2DOptions".
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@122 58c103b0-9528-442c-8745-02648d8b579a

---
+++ src/cpp/torch-nn-conv1doptions.cpp
@@ -0,0 +1,34 @@
+#include <torch/torch.h>
+
+#include <iostream>
+
+// There is no dedicated AdaShadowConv1dOptions object; we simply use
+// torch::nn::Conv1dOptions for this and return pointers to these
+// objects to the Ada side.
+
+// Functions that Ada will call to create an AdaShadowConv1dOptions object
+// and to destroy them. Ada side will own all created objects and
+// manage their lifetime:
+
+extern "C" {
+
+torch::nn::Conv1dOptions* new_AdaShadowConv1dOptions (int64_t x,
+                                                      int64_t y,
+                                                      int64_t ks)
+{
+    // We will handle allocation failure on the Ada side, no need to
+    // throw (raise) and exception here:
+    using namespace std;
+    cout << "Creating a new shadow Conv1dOptions object for Ada variable" << endl;
+    return new (std::nothrow) torch::nn::Conv1dOptions (x, y, ks);
+}
+
+void delete_AdaShadowConv1dOptions (torch::nn::Conv1dOptions* shadow_object)
+{
+    using namespace std;
+    cout << "Deleting the shadow Conv1dOptions object for Ada, object's address is "
+         << shadow_object << endl;
+    delete shadow_object;
+}
+
+}; // extern "C"
