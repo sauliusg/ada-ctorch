dcdd81f4f17e4fa4268923527bb502bcfbb30343
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-03-09 19:41:41 +0000

    Removing an obsolete subtype declartion.
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@142 58c103b0-9528-442c-8745-02648d8b579a

--- src/torch-nn.ads
+++ src/torch-nn.ads
@@ -14,10 +14,6 @@ package Torch.NN is
    
    -- ------------------------------------------------------------------------
    
-   subtype Int64_T is Long_Integer range -2**31 .. 2**31-1;
-   
-   -- ------------------------------------------------------------------------
-   
    type Conv1d_Options is new Ada.Finalization.Limited_Controlled with private;
    
    overriding
