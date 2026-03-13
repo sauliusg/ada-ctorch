247b4217ebd5cce55f547f4caf1c940e3e804728
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-03-13 20:21:31 +0000

    Compiling C++ - side "map" with the automatically deduced type.
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@155 58c103b0-9528-442c-8745-02648d8b579a

--- src/cpp/torch-datasets.cpp
+++ src/cpp/torch-datasets.cpp
@@ -1,4 +1,5 @@
 #include <torch/torch.h>
+#include <torch/data/transforms/tensor.h>
 
 extern "C" {
 
@@ -14,4 +15,14 @@ extern "C" {
         delete dset;
     }
 
+    auto
+    map(torch::data::datasets::MNIST *ds,
+        // torch::data::transforms::TensorTransform<> *transform
+        torch::data::transforms::Normalize<> *transform
+        )
+    {
+        return ds->map(*transform);
+        // return ds->map(torch::data::transforms::Normalize<>(0.1307, 0.3081));
+    }
+    
 }
