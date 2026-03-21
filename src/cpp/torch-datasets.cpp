9ebc13c7d2527d0e7fa02a6c646a5ea07ead4b67
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-03-21 20:51:21 +0000

    Removing functions that deal with templated types and not with their pointers.
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@163 58c103b0-9528-442c-8745-02648d8b579a

--- src/cpp/torch-datasets.cpp
+++ src/cpp/torch-datasets.cpp
@@ -19,16 +19,6 @@ extern "C" {
     torch::data::datasets::MapDataset<
         torch::data::datasets::MNIST,
         torch::data::transforms::Normalize<>
-    >
-    mnist_normalise(torch::data::datasets::MNIST *ds,
-                    torch::data::transforms::Normalize<> *transform)
-    {
-        return ds->map(*transform);
-    }
-    
-    torch::data::datasets::MapDataset<
-        torch::data::datasets::MNIST,
-        torch::data::transforms::Normalize<>
     > *
     new_mnist_normalises(torch::data::datasets::MNIST *ds,
                          torch::data::transforms::Normalize<> *transform)
@@ -40,16 +30,6 @@ extern "C" {
                 torch::data::transforms::Normalize<>
             > (*ds, *transform);
         return ret;
-    }
-    
-    torch::data::datasets::MapDataset<
-        torch::data::datasets::MNIST,
-        torch::data::transforms::Stack<>
-    >
-    mnist_to_tensor(torch::data::datasets::MNIST *ds,
-                    torch::data::transforms::Stack<> *transform)
-    {
-        return ds->map(*transform);
     }
     
     torch::data::datasets::MapDataset<
