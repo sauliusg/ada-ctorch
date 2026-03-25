97149a5cd1b148a50210496db6c7b707944af80d
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-03-25 19:20:54 +0000

    Testing diffrent MNIST load directories and different Ada function to load (and normalise) a dataset.
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@180 58c103b0-9528-442c-8745-02648d8b579a

--- programs/try_datasets.adb
+++ programs/try_datasets.adb
@@ -1,12 +1,16 @@
 with Torch.NN; -- need to satisfy C++ linker, provides Ada callback for C++
 with Torch.Data.Datasets; use Torch.Data.Datasets;
+with Ada.Command_Line; use Ada.Command_Line;
 
 procedure Try_Layers is
    
-   Plain_MNIST_Dataset : MNIST := Make_MNIST ("data/");
+   Root_Dir : String :=
+     (if Argument_Count > 0 then Argument (1) else "data/");
    
-   -- MNIST_Dataset : MNIST :=
-   --   Make_Stacked_Normalised_MNIST ("data/", 0.1307, 0.3081);
+   Plain_MNIST_Dataset : MNIST := Make_MNIST (Root_Dir);
+   
+   MNIST_Dataset : MNIST :=
+     Make_Stacked_Normalised_MNIST (Root_Dir, 0.1307, 0.3081);
    
 begin
    null;
