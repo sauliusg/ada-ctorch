d76134ec9910b622be1f089134b600286cf28899
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-03-27 14:20:53 +0000

    Removing old example code.
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@206 58c103b0-9528-442c-8745-02648d8b579a

--- src/cpp/torch-dataloaders.h
+++ src/cpp/torch-dataloaders.h
@@ -29,19 +29,4 @@ typedef std::unique_ptr<
         >
     data_loader_sequential_sampler_t;
 
-/*
-typedef torch::data::datasets::MapDataset<
-            torch::data::datasets::MNIST,
-            torch::data::transforms::Normalize<>
-        > mnist_normalised_dataset_t;
-
-typedef torch::data::datasets::MapDataset<
-            torch::data::datasets::MapDataset<
-                torch::data::datasets::MNIST,
-                torch::data::transforms::Normalize<>
-            >,
-            torch::data::transforms::Stack<>
-        > mnist_stacked_dataset_t;
-*/
-
 #endif
