295b0185403279980860c35b51ca89682f72cd41
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-04-26 09:45:07 +0000

    Adding the C++ "glue" code to create and delete torch::Device objects.
    trunk/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/trunk@224 58c103b0-9528-442c-8745-02648d8b579a

---
+++ src/cpp/device.cpp
@@ -0,0 +1,23 @@
+// uses:
+#include <torch/torch.h>
+
+extern "C" {
+
+    // This C++ binder does not declare its own class; instead it
+    // allocates and retuers a pointer directly to an allocated
+    // torch::Device object.
+
+    torch::Device*
+    new_torch_device (c10::DeviceType tp, c10::DeviceIndex idx)
+    {
+        return new (std::nothrow) torch::Device (tp, idx);
+    }
+    
+    void
+    detele_torch_device (torch::Device* dev)
+    {
+        delete dev;
+    }
+
+
+}
