e2716b03fddc7d9a4efa4997da5808db9993aa46
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-07-17 15:44:28 +0000

    Adding the dataset-general data loader creation function.
    branches/saulius-ada-abstract-dataloader-implementation/src/cpp/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/branches/saulius-ada-abstract-dataloader-implementation@483 58c103b0-9528-442c-8745-02648d8b579a

--- src/cpp/ada-shadow-dataset.cpp
+++ src/cpp/ada-shadow-dataset.cpp
@@ -1,1 +1,38 @@
+// exports:
 #include <ada-shadow-dataset.hpp>
+
+//uses:
+#include <ada_c_error_code_helpers.h>
+#include <assert.h>
+
+extern "C"
+AdaShadowDataLoader*
+new_ada_shadow_data_loader(
+    AdaShadowDataset* dataset,
+    std::size_t batch_size,
+    Ada_DataLoader_Code sampler_kind,
+    ada_c_error_type* err)
+{
+    try
+    {
+        assert(dataset);
+        
+        AdaDataLoaderOptions options
+            (
+             batch_size,
+             // The below conversion works since the Ada side
+             // Ada_DataLoader_Code values which we get here are
+             // automatically derived from the explicit
+             // Ada_DataLoader_Code_Enum values in the C header,
+             // converted to and .ads file:
+             (Ada_DataLoader_Code_Enum) sampler_kind
+            );
+
+        return dataset->new_ada_shadow_data_loader(options);
+    }
+    catch (...)
+    {
+        handle_exception(err, __FUNCTION__);
+        return nullptr;
+    }
+}
