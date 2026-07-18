006b5e2dbc2199d8aa7ea45f246cf2687d516baa
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-07-18 12:37:47 +0000

    Implementing the missing "new_ada_shadow_iterator_holder()" fuction. Now the code compiles and links. Next, we need to remove the old MNIST data loader implementation and switch to the new, universal dataset implementation in Ada.
    branches/saulius-ada-abstract-dataloader-implementation/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/branches/saulius-ada-abstract-dataloader-implementation@488 58c103b0-9528-442c-8745-02648d8b579a

--- src/cpp/ada-shadow-dataloader.cpp
+++ src/cpp/ada-shadow-dataloader.cpp
@@ -1,1 +1,37 @@
 #include "ada-shadow-dataloader.hpp"
+
+typedef torch::data::Iterator<torch::data::Example<>> example_iterator_t;
+    
+struct AdaShadowIteratorHolder {
+
+    example_iterator_t it;
+
+    AdaShadowIteratorHolder(const example_iterator_t& iter)
+        : it(iter),
+          refcount(1)
+    {
+    }
+
+    void add_reference()
+    {
+        ++refcount;
+    }
+
+    bool release_reference()
+    {
+        return (--refcount) == 0;
+    }
+
+private:
+
+    std::atomic_size_t refcount{1};
+};
+
+
+AdaShadowIteratorHolder*
+new_ada_shadow_iterator_holder(
+    torch::data::Iterator<torch::data::Example<>> iterator)
+{
+    return new AdaShadowIteratorHolder(iterator);
+}
+
