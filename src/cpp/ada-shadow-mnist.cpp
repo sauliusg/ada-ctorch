9efc8b9c21a30b3038efe13f281cbbb6f2320509
saulius <saulius@58c103b0-9528-442c-8745-02648d8b579a>
2026-07-19 10:48:25 +0000

    Defining a class to represent a "raw" MNIST data set.
    branches/saulius-ada-abstract-dataloader-implementation/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ctorch/branches/saulius-ada-abstract-dataloader-implementation@501 58c103b0-9528-442c-8745-02648d8b579a

--- src/cpp/ada-shadow-mnist.cpp
+++ src/cpp/ada-shadow-mnist.cpp
@@ -169,6 +169,42 @@ make_mnist_random_loader(
 // descendants, so that we can present the template-expanded MNIST
 // dataset types using run-time polymorphism:
 
+// "Raw" dataset:
+
+class AdaShadowMNISTDataset final
+    : public AdaShadowDataset
+{
+private:
+
+    MNISTDatasetType dataset_;
+
+public:
+
+    explicit AdaShadowMNISTDataset(const std::string& path):
+        dataset_(make_mnist_dataset(path))
+    {}
+
+    virtual std::size_t size() const
+    {
+        auto s = dataset_.size();
+
+        if (!s) {
+            throw std::runtime_error("MNIST dataset has no defined size.");
+        }
+        
+        return *s;
+    }
+
+    AdaShadowDataLoader*
+    new_ada_shadow_data_loader(const AdaDataLoaderOptions& options) override
+    {
+        throw std::invalid_argument("Only Stack<>() transformed datasets can "
+                                    "currently produce loaders. "
+                                    "This is just a Normalized MNIST dataset.");
+    };
+
+}; // class
+
 // Normalised dataset:
 
 class AdaShadowMNISTNormalisedDataset final
